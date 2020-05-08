7bc6708b95dca1bb4f39c5d7e3b1709e0518c949
Ricardo Bermejo <mestreledark@gmail.com>
2020-05-08 17:27:20 -0300

    Storage initial Rafts

--- src/Storage/Files.php
+++ src/Storage/Files.php
@@ -1,15 +1,23 @@
-    <?php 
-    
-    namespace RBFrameworks\Storage;
-    
-    class Files implements \RBFrameworks\StorageInterface {
-        
-        public function write() {
-            
-        }        
-        
-        public function read() {
-            
-        }
-        
+<?php 
+
+namespace RBFrameworks\Storage;
+
+class Files implements StorageInterface {
+
+    public function exists(string $path): bool {
+        return file_exists($path);
     }
+
+    public function read(string $path): string {
+        return file_get_contents($path);
+    }
+
+    public function remove(string $path): void {
+        unlink($path);
+    }
+
+    public function write(string $path, string $data): void {
+        file_put_contents($path, $dada);
+    }
+
+}
--- src/Storage/StorageInterface.php
+++ src/Storage/StorageInterface.php
@@ -1,11 +1,15 @@
 <?php 
 
-namespace RBFrameworks;
+namespace RBFrameworks\Storage;
 
 interface StorageInterface {
 
-    public function write();
+    public function write(string $path, string $data) : void;
 
-    public function read();
+    public function read(string $path) : string;
 
+    public function exists(string $path) : bool;
+
+    public function remove(string $path) : void;
+    
 }
