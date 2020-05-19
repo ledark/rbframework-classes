76a43ff3b35083e5e0fe85c54c835e851062e53a
Ricardo Bermejo <mestreledark@gmail.com>
2020-05-19 14:12:44 -0300

    new interfaces and utils

---
+++ src/Abstracts/Price.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RBFrameworks\Abstracts;
+
+use Interfaces\Price;
+
+abstract class Price implements Price {
+
+}
--- src/Interfaces/Model.php
+++ src/Interfaces/Model.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Interfaces;
+namespace RBFrameworks\Interfaces;
 
 interface Model {
     
