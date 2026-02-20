0967b30d76ee32aa2cbb7ce6e85bccf3a0350e32
Ricardo Bermejo <mestreledark@gmail.com>
2026-02-20 00:39:05 -0300

    change: udate project inicialization

--- tests/projeto-exemplo/index.php
+++ tests/projeto-exemplo/index.php
@@ -1,12 +1,41 @@
 <?php
 
+use RBFrameworks\Autoload;
+
+//GARANTA QUE OS INCLUDES DO COMPOSER SEJAM CARREGADOS
+
+/*
+require_once "_app/class/composer/autoload.php";
+require_once "_app/class/composer/ledark/rbframeworks/_include.php";
+*/
+
+if(!function_exists('get_root_path')) {
+    throw new \Exception('get_root_path function not found. Create this function in your project.');
+}
+
+//DEFINA A FUNÇÃO get_root_path() PARA CADA PROJETO
+
+/*
+function get_root_path(string $sufix = ""):string {
+    $rootPath = realpath(__DIR__ . '/../../').DIRECTORY_SEPARATOR.ltrim($sufix, '/');
+    return $rootPath;
+}
+*/
 
 try {
-    include('_app/php_autoload.php');
-    include('_app/php_functions.php');
-    include('_app/config.php');
-    include('_app/php_router.php');
-} catch(Throwable $e) {
+
+    Autoload::start();
+    Autoload::forceHttps();
+    Autoload::routeMiddleware();
+    Autoload::routeDirectFiles();
+    Autoload::routeApi(function($request_file, $router) {
+        header('HTTP/1.0 404 Not Found');
+        echo '404 Not Found: '.$request_file;
+        echo $router->getCurrentUri();
+        exit();
+    });
+
+} catch(\Throwable $e) {
     header("HTTP/1.0 501 Not Implemented");
     echo $e->getMessage();
     exit();
